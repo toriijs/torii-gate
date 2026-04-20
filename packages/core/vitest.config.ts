365cff0d8efa2ddd01a4926f37d2c4776197f610
Chathu Vishwaijith <3932230+iamchathu@users.noreply.github.com>
2026-04-21 00:21:00 +0200

    chore: :bug: fix vitest path issue

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
-import { sharedConfig } from '../../vitest.base.js';
+import { sharedConfig } from '../../vitest.base.ts';
 
 export default mergeConfig(
   sharedConfig,
   defineConfig({
     test: {
-      include: ['src/**/*.spec.ts', 'tests/**/*.test.ts'],
+      include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
       exclude: ['tests/adapters/contract.test.ts'],
       coverage: {
         include: ['src/**/*.ts'],
